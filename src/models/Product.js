a6559f85bac9983eaeb99cc2e912aa5b7925aa88
Nhu Man <man.huynh230920@vnuk.edu.vn>
2026-04-13 14:03:19 +0700

    chore: restructure project into backend and frontend folders

--- src/models/Product.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String
-});
-
-export default mongoose.model("Product", productSchema);
