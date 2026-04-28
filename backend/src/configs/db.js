31645e5e4823a2e0348af656859392b764c701ed
CauDravan <kindgate000@gmail.com>
2026-04-28 15:12:45 +0700

    refactor: remove unnecessary comments and improve code readability in db.js

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -4,12 +4,10 @@ const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
 
-    // - Kiểm tra nếu biến môi trường
     if (!mongoURI) {
       throw new Error("MONGODB_URI chưa được cấu hình trong .env");
     }
 
-    // - Kết nối đến MongoDB Atlas
     await mongoose.connect(mongoURI);
     console.log("MongoDB Đã Kết Nối Thành Công!");
   }
